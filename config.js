52ff432f5934627dfec59b35f73f4a31f11323e2
Eltee <l361an@gmail.com>
2026-03-19 01:11:01 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// config.js
+
+export const GROUP_ID = -1001901372111;
+export const LOG_THREAD_ID = 82107;
+
+export const LINK_REGEX =
+  /(https?:\/\/[^\s]+|t\.me\/[^\s]+|telegram\.me\/[^\s]+|wa\.me\/[^\s]+|bit\.ly\/[^\s]+|tinyurl\.com\/[^\s]+)/gi;
+
+export const DEFAULTS = {
+  flood_limit: "5",
+  flood_window: "10",
+  mute_minutes: "60",
+  banned_words: "",
+  link_whitelist: "[]",
+  link_blacklist: "[]",
+  welcome_text: "Selamat Bergabung di *TeMan* 🤍",
+  welcome_media: "",
+  welcome_links: "[]"
+};
+
+export const floodMap = {};
